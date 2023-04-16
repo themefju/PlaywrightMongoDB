07a0170507f5e8485cdb74b479af9506792761d0
themefju <98777676+themefju@users.noreply.github.com>
2023-04-16 15:02:02 +0200

    add mongo find commands

--- utils/mongo/models/options.ts
+++ utils/mongo/models/options.ts
@@ -1,4 +1,11 @@
+import { Document } from 'mongodb';
+
 export interface ConnectOptions {
   uri: string;
   dbName: string;
+}
+
+export interface FindArgs {
+  collection: string;
+  query: Document;
 }
