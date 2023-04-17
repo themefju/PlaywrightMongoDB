4411680689d8b9e839f3b24c3b503340e107be72
themefju <98777676+themefju@users.noreply.github.com>
2023-04-17 17:22:16 +0000

    add mongo aggregate command (#5)

---
+++ utils/mongo/aggregate.ts
@@ -0,0 +1,29 @@
+import { AggregateArgs, ConnectOptions } from './models/options';
+import { defaults } from '../utils';
+import { client, defaultOptions } from './connect';
+
+export async function aggregate(args: AggregateArgs) {
+  const options: ConnectOptions & AggregateArgs = defaults(
+    defaultOptions,
+    args
+  );
+
+  return await client(options.uri)
+    .connect()
+    .then(async (client) => {
+      return await client
+        .db(options.dbName)
+        .collection(options.collection)
+        .aggregate(options.pipeline, options.options)
+        .toArray()
+        .then((result) => {
+          return result;
+        })
+        .catch((err) => {
+          throw err;
+        })
+        .finally(() => {
+          client.close();
+        });
+    });
+}
